43986ce50ea517696c4054e6b345dcb672692819
Dracocybertech <pointdujourkh@gmail.com>
2025-12-29 18:28:04 +0100

    Add const

--- app/services/DrawingDimensions.tsx
+++ app/services/DrawingDimensions.tsx
@@ -2,4 +2,6 @@ import { Dimensions } from "react-native";
 
 export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 export const X_MAX = SCREEN_WIDTH * 0.95
+export const Y_MAX = SCREEN_HEIGHT * 0.95
+export const HALF_Y = (Y_MAX + SCREEN_HEIGHT * 0.301) / 2
 export const SCALE = 40;
